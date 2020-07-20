d63729c500993dc3e092c0bee8893f2123fffb27
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-07-20 20:13:07 +0200

    update app.tsx & index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'css/App.css';
+import pokeball from 'img/pokeball.svg';
+import { PokemonList } from 'components';
 
 function App() {
   return (
     <div className="App">
+      <img
+        src={pokeball}
+        className="App-logo"
+        alt="pokeball"
+        aria-hidden="true"
+      />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pokedex</h1>
       </header>
+      <main className="App-main">
+        <PokemonList />
+      </main>
     </div>
   );
 }
