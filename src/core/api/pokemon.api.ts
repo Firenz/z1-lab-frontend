ea02d49e185074bbd15ba0fbc9cca3c92be685dd
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-07-20 20:11:51 +0200

    add core folder with api and vm methods

---
+++ src/core/api/pokemon.api.ts
@@ -0,0 +1,27 @@
+export interface PokemonApi {
+  id: string;
+  name: string;
+  sprites: PokemonApiSprites;
+  types: PokemonApiTypes[];
+}
+
+export interface PokemonApiSprites {
+  back_default: string | null;
+  back_female: string | null;
+  back_shiny: string | null;
+  back_shiny_female: string | null;
+  front_default: string | null;
+  front_female: string | null;
+  front_shiny: string | null;
+  front_shiny_female: string | null;
+}
+
+export interface PokemonApiTypes {
+  slot: number;
+  type: { name: string; url: string };
+}
+
+export const getPokemonApi = async (pokemonId: number) => {
+  const fetchUrl = `http://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
+  return fetch(fetchUrl).then((response) => response.json());
+};
---
+++ src/core/model/pokemon.vm.ts
@@ -0,0 +1,35 @@
+import {
+  PokemonApi,
+  PokemonApiTypes,
+  PokemonApiSprites,
+} from './../api/pokemon.api';
+export interface PokemonVm {
+  id: string;
+  name: string;
+  types: string[];
+  spriteUrl: string;
+}
+
+export const mapperPokemonApiToVm = (pokemonApi: PokemonApi): PokemonVm => {
+  console.log(pokemonApi.types[0]);
+  const type1: PokemonApiTypes = pokemonApi.types[0];
+  let type2: PokemonApiTypes = pokemonApi.types[1];
+  if (type2 == null) {
+    type2 = {
+      slot: 1,
+      type: {
+        name: '',
+        url: '',
+      },
+    } as PokemonApiTypes;
+  }
+
+  const pokemonVm: PokemonVm = {
+    id: pokemonApi.id,
+    name: pokemonApi.name,
+    types: [type1.type.name, type2.type.name],
+    spriteUrl: pokemonApi.sprites.front_default || '',
+  };
+
+  return pokemonVm;
+};
